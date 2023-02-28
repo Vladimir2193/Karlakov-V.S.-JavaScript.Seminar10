a7a90eb79454e9febabdf611a76f2d6b07075b7f
Vladimir <k.vovik15@mail.ru>
2023-02-28 17:19:27 +0300

    First commit

---
+++ data.js
@@ -0,0 +1,32 @@
+const userJsonData = `
+{
+    "first": {
+    "ip": "77.94.106.38",
+    "hostname": "h77-94-106-38.dyn.bashtel.ru",
+    "city": "Uchaly",
+    "region": "Bashkortostan Republic",
+    "country": "RU",
+    "loc": "54.3392,59.3094",
+    "org": "AS28812 PJSC Bashinformsvyaz",
+    "postal": "453740",
+    "timezone": "Asia/Yekaterinburg",
+    "readme": "https://ipinfo.io/missingauth",
+    "paragraph": "Параграф из файла JSON",
+    "images": "https://avatars.mds.yandex.net/get-games/1892995/2a00000185c0354a9e53a9527c755843d354/orig"
+  },
+    "second": {
+    "post code": "33162",
+    "country": "United States",
+    "country abbreviation": "US",
+    "title": "Заголовок из JSON",
+    "places": [
+      {
+        "place name": "Miami",
+        "longitude": "-80.183",
+        "state": "Florida",
+        "state abbreviation": "FL",
+        "latitude": "25.9286"
+      }
+    ]
+  }
+}`
